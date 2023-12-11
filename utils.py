01296ef632699f253172a60f00ee5fbe6ba611f3
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-11 14:29:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -91,7 +91,7 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
-                await reply.edit(f'`\n ╭──⌯════Bot Statics═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n╰─═══ ✪Powered By @Rishavdevkrr ✪ ═══─╯`\n') 
+                await reply.edit(f'`\n ╭──⌯══════Bot Statics═════════⌯────╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n╰─═══ ✪Powered By @Rishavdevkrr ✪ ═══─╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
 
