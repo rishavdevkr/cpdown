39c21333b7d320d449d08fc2d98d93da600c0bd1
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-11 16:49:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,7 @@
 api_id = 22779671
 api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
 bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
+auth_users = [1713924419]
+sudo_user = 1713924419
+log_channel = -1002143629860
+txt_channel = -1002049391187
