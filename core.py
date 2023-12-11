2474628388e5b67cfacec34daa88c9480c98c9a1
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-11 15:03:26 +0530

    Update core.py

--- core.py
+++ core.py
@@ -152,7 +152,7 @@ def time_name():
 
 
 async def download_video(url,cmd, name):
-    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
+    download_cmd = f'{YTDLP} --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32 -s 16 -k 1M"'
     global failed_counter
     print(download_cmd)
     logging.info(download_cmd)
