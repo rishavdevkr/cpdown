77640c8948574c91ec69673700c022b108253d7f
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2024-07-17 12:35:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,223 @@
+import os
+import re
+import sys
+import json
+import time
+import asyncio
+import requests
+import subprocess
+import core as helper
+from utils import progress_bar
+from vars import api_id, api_hash, bot_token, auth_users, sudo_user, log_channel, txt_channel
+from aiohttp import ClientSession
+from pyromod import listen
+from subprocess import getstatusoutput
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait
+from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
+from pyrogram.types.messages_and_media import message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+bot = Client(
+	"bot",
+	api_id=api_id,
+	api_hash=api_hash,
+	bot_token=bot_token)
+
+@bot.on_message(filters.command(["start"]))
+async def account_login(bot: Client, m: Message):
+	editable = await m.reply_text("Hi!\n\nGive /classplus Command and send me Text file to Downlaod.\n")
+
+@bot.on_message(filters.command("restart"))
+async def restart_handler(_, m):
+	await m.reply_text("**I'm Restarted**🚦", True)
+	os.execl(sys.executable, sys.executable, *sys.argv)
+
+@bot.on_message(filters.command("terms"))
+async def terms_han(bot: Client, m: Message):
+	
+	await m.reply_text("Dear user,\n\nWelcome to our video downloader bot on Telegram. Before you start using our bot, please read these terms and conditions carefully.\n\nBy using our bot, you agree to the following terms and conditions:\n\n1. Our bot is intended for personal, non-commercial use only. You are responsible for any content that you download through our bot and you should ensure that you have the necessary permissions and rights to use and share the content.\n\n2. Downloading copyrighted content through our bot is strictly prohibited. If we receive any complaints of copyright infringement, we reserve the right to take down the infringing content and terminate the user's access to our bot.\n\n3. We do not store any of your personal data or download history. Your privacy and security are important to us, and we have taken all necessary measures to ensure that your information is safe and protected.\n\n4. We reserve the right to suspend or terminate the bot's services at any time and for any reason.\n\n5. By using our bot, you agree to indemnify and hold us harmless from any claims, damages,\nor losses arising from your use of our bot.\n\nIf you have any questions or concerns about our terms and conditions, please contact us.\n\nThank you for using our video downloader bot on Telegram.\n\nBest regards,\nRishav")
+	
+@bot.on_message(filters.command(["classplus"]))
+async def account_login(bot: Client, m: Message):
+	editable = await m.reply_text('Hi\n\nI am Ready to download send me a text file» ')
+	input: Message = await bot.listen(editable.chat.id)
+	x = await input.download()
+	await input.delete(True)
+
+	path = f"./downloads/{m.chat.id}"
+
+	try:
+	   with open(x, "r") as f:
+		   content = f.read()
+	   content = content.split("\n")
+	   links = []
+	   for i in content:
+		   links.append(i.split("://", 1))
+	   os.remove(x)
+			# print(len(links)
+	except:
+		   await m.reply_text("Invalid file input.")
+		   os.remove(x)
+		   return
+	
+   
+	await editable.edit(f"Total links founds in Text File are **{len(links)}**\n\nSend From where you want to download initial is **1**")
+	input0: Message = await bot.listen(editable.chat.id)
+	raw_text = input0.text
+	await input0.delete(True)
+
+	await editable.edit("**Enter Batch Name**")
+	input1: Message = await bot.listen(editable.chat.id)
+	raw_text0 = input1.text
+	await input1.delete(True)
+	
+
+	await editable.edit("**Enter resolution**")
+	input2: Message = await bot.listen(editable.chat.id)
+	raw_text2 = input2.text
+	await input2.delete(True)
+	try:
+		if raw_text2 == "144":
+			res = "256x144"
+		elif raw_text2 == "240":
+			res = "426x240"
+		elif raw_text2 == "360":
+			res = "640x360"
+		elif raw_text2 == "480":
+			res = "854x480"
+		elif raw_text2 == "720":
+			res = "1280x720"
+		elif raw_text2 == "1080":
+			res = "1920x1080" 
+		else: 
+			res = "UN"
+	except Exception:
+			res = "UN"
+	
+	
+
+	await editable.edit("Enter A Caption or Downloded by to add Otherwise send **no**")
+	input3: Message = await bot.listen(editable.chat.id)
+	raw_text3 = input3.text
+	await input3.delete(True)
+	highlighter  = f"️ ⁪⁬⁮⁮⁮"
+	if raw_text3 == 'no':
+		MR = highlighter 
+	else:
+		MR = raw_text3
+   
+	await editable.edit("Now send the **Thumb url**\nEg » ```https://telegra.ph/file/0633f8b6a6f110d34f044.jpg```\n\nor Send `no`")
+	input6 = message = await bot.listen(editable.chat.id)
+	raw_text6 = input6.text
+	await input6.delete(True)
+	await editable.delete()
+
+	thumb = input6.text
+	if thumb.startswith("http://") or thumb.startswith("https://"):
+		getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+		thumb = "thumb.jpg"
+	else:
+		thumb == "no"
+
+	if len(links) == 1:
+		count = 1
+	else:
+		count = int(raw_text)
+
+	try:
+		for i in range(count - 1, len(links)):
+
+			V = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","") # .replace("mpd","m3u8")
+			url = "https://" + V
+
+			if "visionias" in url:
+				async with ClientSession() as session:
+					async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
+						text = await resp.text()
+						url = re.search(r"(https://.*?playlist.m3u8.*?)\"", text).group(1)
+		
+			if "videos.classplusapp" in url:
+					headers = {'Host': 'api.classplusapp.com', 'x-access-token': raw_text_tok, 'user-agent': 'Mobile-Android', 'app-version': '1.4.37.1', 'api-version': '18', 'device-id': '5d0d17ac8b3c9f51', 'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30', 'accept-encoding': 'gzip'}
+					params = (('url', f'{url}'),)
+					response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
+					url = response.json()['url']
+			elif "media-cdn-alisg.classplusapp.com" in url:
+					headers = {'Host': 'api.classplusapp.com', 'x-access-token': raw_text_tok, 'user-agent': 'Mobile-Android', 'app-version': '1.4.37.1', 'api-version': '18', 'device-id': '5d0d17ac8b3c9f51', 'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30', 'accept-encoding': 'gzip'}
+					params = (('url', f'{url}'),)
+					response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
+					url = response.json()['url']
+			elif "tencdn.classplusapp" in url:
+					headers = {'Host': 'api.classplusapp.com', 'x-access-token': raw_text_tok, 'user-agent': 'Mobile-Android', 'app-version': '1.4.37.1', 'api-version': '18', 'device-id': '5d0d17ac8b3c9f51', 'device-details': '2848b866799971ca_2848b8667a33216c_SDK-30', 'accept-encoding': 'gzip'}
+					params = (('url', f'{url}'),)
+					response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
+					url = response.json()['url']
+
+			elif '/master.mpd' in url:
+			 id =  url.split("/")[-2]
+			 url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
+
+			name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+			name = f'{str(count).zfill(3)}) {name1[:60]}'
+
+			if "youtu" in url:
+				ytf = f"b[height<={raw_text2}][ext=mp4]/bv[height<={raw_text2}][ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
+			else:
+				ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
+
+			if "jw-prod" in url:
+				cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
+			else:
+				cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
+
+			try:  
+				
+				cc = f'** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.mkv\n**Batch »** {raw_text0}\n\n'
+				cc1 = f'** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**Batch »** {raw_text0}\n\n'
+				if "drive" in url:
+					try:
+						ka = await helper.download(url, name)
+						copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
+						count+=1
+						os.remove(ka)
+						time.sleep(1)
+					except FloodWait as e:
+						await m.reply_text(str(e))
+						time.sleep(e.x)
+						continue
+				
+				elif ".pdf" in url:
+					try:
+						cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+						download_cmd = f"{cmd} -R 25 --fragment-retries 25"
+						os.system(download_cmd)
+						copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
+						count += 1
+						os.remove(f'{name}.pdf')
+					except FloodWait as e:
+						await m.reply_text(str(e))
+						time.sleep(e.x)
+						continue
+				else:
+					Show = f"**⥥ Downloading »**\n\n**Name »** `{name}\nQuality » {raw_text2}`\n\n**Piracy is illegal 🚫**\n\nEnter /terms To know our terms and conditions."
+					prog = await m.reply_text(Show)
+					res_file = await helper.download_video(url, cmd, name)
+					filename = res_file
+					await prog.delete(True)
+					await helper.send_vid(bot, m, cc, filename, thumb, name, prog)
+					count += 1
+					time.sleep(1)
+
+			except Exception as e:
+				await m.reply_text(
+					f"**downloading Interupted **\n{str(e)}\n**Name** » {name}\n**Link** » `{url}`"
+				)
+				continue
+
+	except Exception as e:
+		await m.reply_text(e)
+	await m.reply_text("Done")
+
+
+bot.run()
