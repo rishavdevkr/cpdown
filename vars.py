1d7fece0cbace36a51c97523934f5b51515f9f72
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2024-07-17 11:57:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 25218674
 api_hash = "87c231f0e8704795b8239d06965b4351"
-bot_token = "7198600769:AAFP9zhEs0np54EjC-SGuLBPQHJQilCn0bk"
+bot_token = "7198600769:AAEA_EbrLFOYhMBm7pRi7BWKQgYHFdiIeTg"
 auth_users = [999741495]
 sudo_user = 1713924419
 log_channel = -1002236425890
