a365312c016c34ff69c5f3fe71a18f73c453079a
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2024-08-12 16:17:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
 api_id = 25218674
 api_hash = "87c231f0e8704795b8239d06965b4351"
-bot_token = "7198600769:AAEA_EbrLFOYhMBm7pRi7BWKQgYHFdiIeTg"
+bot_token = "7198600769:AAHfVi7qTaMw20HbpvPrF-fnP77-sWdOthI"
 auth_users = [999741495]
 sudo_user = 1713924419
-log_channel = -1002236425890
-txt_channel = -1002236425890
+log_channel = -1002154281532
+txt_channel = -1002154281532
